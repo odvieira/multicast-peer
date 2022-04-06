5a747530ba03348eba028482329e3fe804235283
Daniel Vieira <dvieira@alunos.utfpr.edu.br>
2022-04-06 16:44:29 -0300

    funcioanndo reqs

--- MulticastPeer.py
+++ MulticastPeer.py
@@ -6,300 +6,277 @@ import struct
 import sys
 from time import time
 
+from async_generator import yield_
+
 
 class MulticastPeer():
-	"""
-	Multicast Peer
-	----------------
-	Communication protocol
+    """
+    Multicast Peer
+    ----------------
+    Communication protocol
 
-	Every package sent must be formated as follows:
-			[ID] [TIME] [STATUS]
-	"""
+    Every package sent must be formated as follows:
+                                    [ID] [TIME] [STATUS]
+    """
 
-	def __init__(self, id, pipe_end: multiprocessing.Pipe = None, \
-		group: str = '228.5.6.7', port: int = 6789):
-		self.group = group
-		self.port = port
-		self.state = 'RELEASED'
-		self.queue = []
-		self.group_members = []
-		self.id = str(id)
-		self.responses = []
-		self.pipe_end = pipe_end
-		self.request_time = -1.0
+    def __init__(self, id, pipe_end: multiprocessing.Pipe = None,
+                 group: str = '228.5.6.7', port: int = 6789):
+        self.group = group
+        self.port = port
+        self.state = 'RELEASED'
+        self.queue = []
+        self.group_members = []
+        self.id = str(id)
+        self.responses_to_rlock = []
+        self.pipe_end = pipe_end
+        self.request_time = -1.0
+        self.inputs = []
+        self.outputs = []
 
-	async def join(self):
-		multicast_group = (self.group, self.port)
+        self.multicast_group = (self.group, self.port)
 
-		# Create the datagram socket
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Lock requester
+        self.create_lock_sock()
 
-		# The socket does not block
-		# indefinitely when trying to receive data.
-		sock.setblocking(0)
+        # Listener
+        self.create_listener()
 
-		# Set the time-to-live for messages to 1 so they do not
-		# go past the local network segment.
-		ttl = struct.pack('b', 1)
-		sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+        # Create the datagram socket
+        self.join_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-		# Send data to the multicast group
-		sent = sock.sendto(
-			'{0} {1} {2}'.format(
-				self.id, time(), 'JOIN').encode(), multicast_group)
+        # The socket does not block
+        # indefinitely when trying to receive data.
+        self.join_sock.setblocking(0)
 
-		# Sockets from which we expect to read
-		inputs=[sock]
+        # Set the time-to-live for messages to 1 so they do not
+        # go past the local network segment.
+        ttl = struct.pack('b', 1)
+        self.join_sock.setsockopt(
+            socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-		# Sockets to which we expect to write
-		outputs=[]
+        return
 
-		while inputs:
-			readable, writable, exceptional = select.select(\
-				inputs, outputs, inputs, 0)
-			
-			if len(readable) == 0:
-				await asyncio.sleep(0.01)
+    def create_lock_sock(self):
+        # Create the datagram socket
+        self.lock_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-			for s in readable:
-				if s is sock:
-					try:
-						data, address=s.recvfrom(4096)
-					except:
-						raise
-					else:
-						if not data:
-							await asyncio.sleep(0.01)
-							continue
+        # The socket does not block
+        # indefinitely when trying to receive data.
+        self.lock_sock.setblocking(0)
 
-						received_message=data.decode().split(' ')
+        # Set the time-to-live for messages to 1 so they do not
+        # go past the local network segment.
+        ttl = struct.pack('b', 1)
+        self.lock_sock.setsockopt(
+            socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-						res={}
+    def create_listener(self) -> int:
+        multicast_group = self.group
+        server_address = (self.group, self.port)
 
-						res['id']=received_message[0]
-						res['time']=received_message[1]
-						res['status']=received_message[2]
-						res['address']=address
+        # Create the socket
+        sock = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-					if res['id'] not in self.group_members:
-						self.group_members.append(res['id'])
+        # Non blocking socket
+        sock.setblocking(0)
 
-	async def active(self):
-		while True:
-			if self.pipe_end.poll():
-				try:
-					command_received = self.pipe_end.recv()
-				except EOFError:
-					self.pipe_end.close()
-					exit(0)
-				else:
-					if command_received == 'JOIN':
-						await self.join()
-					if command_received == 'RLOCK':
-						await self.request_lock()
+        # Bind to the server address
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(server_address)
 
-			else:
-				await asyncio.sleep(0.2)
+        # Tell the operating system to add the socket to
+        # the multicast group on all interfaces.
+        group = socket.inet_aton(multicast_group)
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        sock.setsockopt(
+            socket.IPPROTO_IP,
+            socket.IP_ADD_MEMBERSHIP,
+            mreq)
 
-	def release_lock(self):
-		pass
+        self.listener_socket = sock
 
-	async def request_lock(self, verbose_flag: bool = True) -> int:
-		"""
-		Args:
-			message (bytes): Every package sent must be formated as follows
-								[ID] [TIME] [STATUS]
+        self.inputs.append(self.listener_socket)
 
-			verbose_flag (bool, optional): Print to the output internal states. Defaults to True.
+        return 0
 
-		Returns:
-			int:    0 - all responses were received as expected
-							1 - responses number were below the expected
-		"""
+    async def join(self):
+        # Send data to the multicast group
+        sent = self.join_sock.sendto(
+            '{0} {1} {2}'.format(
+                self.id, time(), 'JOIN').encode(), self.multicast_group)
 
-		self.state = 'WANTED'
-		self.request_time = time()
+        # Sockets from which we expect to read
+        self.inputs.append(self.join_sock)
 
-		msg = '{0} {1} {2}'.format(
-		self.id, self.request_time,
-		self.state).encode()
+    async def active(self):
+        while True:
+            if self.pipe_end.poll():
+                try:
+                    command_received = self.pipe_end.recv()
+                except EOFError:
+                    self.pipe_end.close()
+                    exit(0)
+                else:
+                    if command_received == 'JOIN':
+                        await self.join()
+                    if command_received == 'RLOCK':
+                        await self.request_lock()
+            else:
+                await asyncio.sleep(0.1)
 
-		multicast_group = (self.group, self.port)
+    def release_lock(self):
+        pass
 
-		# Create the datagram socket
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    async def request_lock(self, verbose_flag: bool = True) -> None:
+        """
+        Args:
+                        verbose_flag (bool, optional): Print to the output internal
+                                                        states. Defaults to True.
+        """
 
-		# The socket does not block
-		# indefinitely when trying to receive data.
-		sock.setblocking(0)
+        self.state = 'WANTED'
+        self.request_time = time()
 
-		# Set the time-to-live for messages to 1 so they do not
-		# go past the local network segment.
-		ttl = struct.pack('b', 1)
-		sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+        msg = '{0} {1} {2}'.format(
+            self.id, self.request_time,
+            self.state).encode()
 
-		try:
-			# Send data to the multicast group
-			sent = sock.sendto(msg, multicast_group)
-		except:
-			raise
-		else:
-			# Sockets from which we expect to read
-			inputs=[sock]
+        try:
+            # Send data to the multicast group
+            sent = self.lock_sock.sendto(msg, self.multicast_group)
+        except:
+            raise
+        else:
+            self.inputs.append(self.lock_sock)
 
-			# Sockets to which we expect to write
-			outputs=[]
+    async def listen(self, verbose_flag: bool = True) -> None:
+        """
+        Args:
+                        verbose_flag (bool, optional): Prints internal state.
+                        Defaults to True.
 
-			while inputs:
-				readable, writable, exceptional = select.select(\
-					inputs, outputs, inputs, 0)
-				
-				if len(readable) == 0:
-					await asyncio.sleep(0.01)
+        Every package are formated as follows:
+                        [ID] [TIME] [STATUS]
+        """
 
-				for s in readable:
-					if s is sock:
-						try:
-							data, address=s.recvfrom(4096)
-						except:
-							raise
-						else:
-							if not data:
-								await asyncio.sleep(0.01)
-								continue
+        while True:
+            # Wait for at least one of the sockets to be
+            # ready for processing
 
-							received_message=data.decode().split(' ')
+            if not len(self.inputs) > 0:
+                await asyncio.sleep(0.01)
 
-							res={}
+            try:
+                readable, writable, exceptional = select.select(
+                    self.inputs, self.outputs, self.inputs, 0)
+            except:
+                raise
 
-							res['id']=received_message[0]
-							res['time']=received_message[1]
-							res['status']=received_message[2]
-							res['address']=address
+            if not len(readable) > 0:
+                await asyncio.sleep(0.01)
 
-							if res['status'] == 'WANTED':
-								if self.state == 'HELD' or \
-										(self.state == 'WANTED' and \
-											self.request_time < res['time'] and \
-												self.request_time > 0):
-												# self.request_time > 0 because it was initializes
-												#  with -1
-									self.queue.append(res)
-								else:
-									sock.sendto(
-										'{0} {1} {2}'.format(
-											self.id, time(), self.state),
-										address)
+            for s in readable:
+                try:
+                    data, address = s.recvfrom(4096)
+                except:
+                    raise
+                else:
+                    if verbose_flag:
+                        print('\nPeer [{0}]:'.format(self.id), end=' ')
+                        print('received {} bytes from {}'.format(
+                            len(data), address))
+                        print(data)
 
-							self.responses.append(res)
+                    received_message = data.decode().split(' ')
 
-							if len(self.responses) == len(self.group_members):
-								self.state = 'HELD'
-								self.responses = []
-								inputs.remove(sock)
+                    res = {}
 
-		finally:
-			sock.close()
-		
-		return 0
+                    res['id'] = received_message[0]
+                    res['time'] = float(received_message[1])
+                    res['status'] = received_message[2]
+                    res['address'] = address
 
+                if s is self.listener_socket:
+                    if res['status'] == 'WANTED':
+                        if self.state == 'HELD' or \
+                                (self.state == 'WANTED' and
+                                 self.request_time < res['time'] and
+                                 self.request_time > 0):
+                            # self.request_time > 0 because it was initializes
+                            #  with -1
 
-	async def listen(self, verbose_flag: bool=True) -> None:
-		"""
-		Args:
-			verbose_flag (bool, optional): Prints internal state.
-			Defaults to True.
+                            self.add_to_queue(res)
 
-		Raises:
-			Exception: Unknow
+                            if verbose_flag:
+                                print('\nPeer [{0}]:'.format(self.id), end=' ')
+                                print(address,
+                                      '({0}) joined the queue for the resource'
+                                      .format(res['id']))
 
-		Every package are formated as follows:
-			[ID] [TIME] [STATUS]
-		"""
-		multicast_group=self.group
-		server_address=(self.group, self.port)
+                        else:
+                            self.lock_sock.sendto(
+                                '{0} {1} {2}'.format(
+                                    self.id, time(), self.state).encode(),
+                                address)
 
-		# Create the socket
-		sock=socket.socket(
-			socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+                    elif res['status'] == 'JOIN' and \
+                            res['id'] != self.id and \
+                            res['id'] not in self.group_members:
 
-		# Non blocking socket
-		sock.setblocking(0)
+                        self.group_members.append(res['id'])
 
-		# Bind to the server address
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		sock.bind(server_address)
+                        if verbose_flag:
+                            print('\nPeer [{0}]:'.format(self.id), end=' ')
+                            print(address, '({0}) joined the group'.format(
+                                res['id']))
 
-		# Tell the operating system to add the socket to
-		# the multicast group on all interfaces.
-		group=socket.inet_aton(multicast_group)
-		mreq=struct.pack('4sL', group, socket.INADDR_ANY)
-		sock.setsockopt(
-			socket.IPPROTO_IP,
-			socket.IP_ADD_MEMBERSHIP,
-			mreq)
+                        self.listener_socket.sendto('{0} {1} {2}'.format(
+                            self.id, time(), 'ACK').encode(), address)
 
-		# Sockets from which we expect to read
-		inputs=[sock]
+                    elif res['id'] != self.id and res['status'] != 'ACK':
+                        self.listener_socket.sendto('{0} {1} {2}'.format(
+                            self.id, time(), 'ACK').encode(), address)
 
-		# Sockets to which we expect to write
-		outputs=[]
+                elif s is self.join_sock:
+                    if res['id'] not in self.group_members:
+                        self.group_members.append(res['id'])
 
-		while inputs:
-			# Wait for at least one of the sockets to be
-			# ready for processing
+                    self.inputs.remove(self.join_sock)
 
-			try:
-				readable, writable, exceptional = select.select(\
-					inputs, outputs, inputs, 0)
-			except ValueError:
-				pass
+                elif s is self.lock_sock:
+                    if res['status'] == 'WANTED':
+                        if self.state == 'HELD' or \
+                                (self.state == 'WANTED' and
+                                 self.request_time < res['time'] and
+                                 self.request_time > 0):
+                            # self.request_time > 0 because it was initializes
+                            #  with -1
 
-			if len(readable) == 0:
-				await asyncio.sleep(0.01)
+                            self.add_to_queue(res)
 
-			for s in readable:
-				if s is sock:
-					try:
-						data, address=s.recvfrom(4096)
+                        else:
+                            self.lock_sock.sendto(
+                                '{0} {1} {2}'.format(
+                                    self.id, time(), self.state),
+                                address)
 
-						if not data:
-							await asyncio.sleep(0.01)
-							continue
+                        # Only count responsed to my request
+                        self.responses_to_rlock.append(res)
 
-						received_message=data.decode().split(' ')
+                        if len(self.responses_to_rlock) == len(
+                                self.group_members):
 
-						newmsg={}
+                            self.state = 'HELD'
+                            self.responses_to_rlock = []
+                            self.inputs.remove(self.lock_sock)
 
-						newmsg['id']=received_message[0]
-						newmsg['time']=received_message[1]
-						newmsg['status']=received_message[2]
-						newmsg['address']=address
+    def add_to_queue(self, request: dict):
+        already_in_the_queue = False
 
-						if newmsg['status'] == 'JOIN' and \
-								newmsg['id'] != self.id and \
-								newmsg['id'] not in self.group_members:
+        for e in self.queue:
+            if request['id'] == e['id']:
+                already_in_the_queue = True
 
-							self.group_members.append(newmsg['id'])
-
-							if verbose_flag:
-								print('\nPeer [{0}]:'.format(self.id), end=' ')
-								print(address, '({0}) joined the group'.format(
-									newmsg['id']))
-
-							sock.sendto('{0} {1} {2}'.format(self.id, time(), 'ACK')
-										.encode(), address)
-
-						elif newmsg['id'] != self.id and newmsg['status'] != 'ACK':
-							if verbose_flag:
-								print('\nPeer [{0}]:'.format(self.id), end=' ')
-								print('received {} bytes from {}'.format(
-									len(data), address))
-								print(data)
-
-							sock.sendto('{0} {1} {2}'.format(self.id, time(), 'ACK')
-										.encode(), address)
-
-					except:
-						raise
+        if not already_in_the_queue:
+            self.queue.append(request)
--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 import asyncio
 import multiprocessing
 from random import randint
+import tkinter
+from tkinter import messagebox
 
 from MulticastPeer import MulticastPeer
 
-async def send_command(c:str, p:multiprocessing.Pipe) -> int:
-	p.send(c)
+peer_pipe_rcv, peer_pipe_snd = multiprocessing.Pipe(duplex=False)
 
-	return 0
+
+@asyncio.coroutine
+def run_tk(root, interval=0.1):
+    try:
+        while True:
+            root.update()
+            yield from asyncio.sleep(interval)
+    except tkinter.TclError as e:
+        exit(0)
+        # if "application has been destroyed" not in e.args[0]:
+        # 	raise
+
+
+def send_command_join() -> None:
+    peer_pipe_snd.send('JOIN')
+
+
+def send_command_request() -> None:
+    peer_pipe_snd.send('RLOCK')
+
 
 async def main():
-    peer_pipe_rcv, peer_pipe_snd =  multiprocessing.Pipe(duplex=False) 
+    root_obj = tkinter.Tk()
 
-    peer = MulticastPeer(id=randint(0,100), pipe_end=peer_pipe_rcv)
+    button_join = tkinter.Button(
+        root_obj, text="Join", command=send_command_join)
+    button_join.pack(side='left', expand=1)
+
+    button_request = tkinter.Button(
+        root_obj, text="Request", command=send_command_request)
+    button_request.pack(side='left', expand=1)
+
+    peer = MulticastPeer(id=randint(0, 100), pipe_end=peer_pipe_rcv)
 
     await asyncio.gather(
         peer.listen(),
         peer.active(),
-				send_command('JOIN', peer_pipe_snd),
+        run_tk(root_obj, 0.1)
     )
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-    
